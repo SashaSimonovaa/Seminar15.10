f7b2f88d955b714eb48d1286784158e6a30ca10b
SashaSimonovaa <sasasimonova429@gmail.com>
2022-10-22 14:21:03 +0300

    снежинка

--- 2.py
+++ 2.py
@@ -1,2 +1,21 @@
 import turtle
 
+def f(len, gl):
+    if gl == 0:
+        turtle.fd(len)
+    else:
+        f(len//3, gl-1)
+        turtle.left(60)
+        f(len//3, gl-1)
+        turtle.right(120)
+        f(len//3, gl-1)
+        turtle.left(60)
+        f(len//3, gl-1)
+
+turtle.shape("turtle")
+
+for i in range(3):
+    f(200, 3)
+    turtle.right(120)
+
+turtle.done()
